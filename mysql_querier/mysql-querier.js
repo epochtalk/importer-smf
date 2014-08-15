30e54f3f14b9548fae1ba6f9e6962a38c6a267f2
unenglishable <unenglishable@gmail.com>
2014-08-14 22:24:02 -1000

    whitespace fix for mysql-querier
    fix indentation as well as extra space after function
    function () ->
    function()

--- mysql_querier/mysql-querier.js
+++ mysql_querier/mysql-querier.js
@@ -7,38 +7,38 @@ module.exports = function(config) {
 }
 
 var MysqlQuerier = {
-  getTables: function (callback) {
-               var tables = [];
+  getTables: function(callback) {
+    var tables = [];
 
-               pool.query('SHOW tables', function (err, rows) {
-                 rows.forEach(function (row) {
-                   tables.push(row[Object.keys(row)[0]]);
-                 });
-                 return callback(err, tables);
-               });
-             },
-  getColumns: function (table, callback) {
-                pool.query('SHOW columns FROM ??', table, function (err, rows) {
-                  return callback(err, rows);
-                });
-              },
-  getRowsWhere: function (table, obj, callback) {
-                  pool.query('SELECT * FROM ?? WHERE ?', [table, obj], function (err, rows) {
-                    return callback(err, rows);
-                  });
-                },
-  createRowStream: function (table) {
-                     return pool.query('SELECT * FROM ??', table).stream();
-                   },
-  createRowStreamWhere: function (table, obj) {
-                          return pool.query('SELECT * FROM ?? WHERE ?', [table,obj]).stream();
-                        },
-  end: function (callback) {
-         if (callback && typeof(callback) === "function") {
-           pool.end(callback());
-         }
-         else {
-           pool.end();
-         }
-       }
+    pool.query('SHOW tables', function(err, rows) {
+      rows.forEach(function(row) {
+        tables.push(row[Object.keys(row)[0]]);
+      });
+      return callback(err, tables);
+    });
+  },
+  getColumns: function(table, callback) {
+    pool.query('SHOW columns FROM ??', table, function(err, rows) {
+      return callback(err, rows);
+    });
+  },
+  getRowsWhere: function(table, obj, callback) {
+    pool.query('SELECT * FROM ?? WHERE ?', [table, obj], function(err, rows) {
+      return callback(err, rows);
+    });
+  },
+  createRowStream: function(table) {
+    return pool.query('SELECT * FROM ??', table).stream();
+  },
+  createRowStreamWhere: function(table, obj) {
+    return pool.query('SELECT * FROM ?? WHERE ?', [table,obj]).stream();
+  },
+  end: function(callback) {
+    if (callback && typeof(callback) === "function") {
+      pool.end(callback());
+    }
+    else {
+      pool.end();
+    }
+  }
 }
