2441bc88ba4299bc2fde843761dcc746959ab74f
unenglishable <unenglishable@gmail.com>
2014-08-14 22:51:15 -1000

    whitespace fix for mysql_querier
    fix function spacing

--- mysql_querier/test.js
+++ mysql_querier/test.js
@@ -7,62 +7,61 @@ var through2 = require('through2');
 
 var tests = {
   getTables: function(callback) {
-           mQ.getTables(function(err, tables) {
-             if (err) {
-               console.log(err);
-             }
+    mQ.getTables(function(err, tables) {
+      if (err) {
+        console.log(err);
+      }
 
-             console.log('testGetTables:\n'+tables);
-             callback();
-           });
-         },
+    console.log('testGetTables:\n'+tables);
+      callback();
+    });
+  },
   getColumns: function(callback) {
-            mQ.getColumns('test_table', function(err, columns) {
-              if (err) {
-                console.log(err);
-              }
+    mQ.getColumns('test_table', function(err, columns) {
+      if (err) {
+        console.log(err);
+      }
 
-              console.log('testGetColumns:');
-              console.log(columns);
-              callback();
-            });
-          },
+      console.log('testGetColumns:');
+      console.log(columns);
+      callback();
+    });
+  },
   getRowsWhere: function(callback) {
-                  var rowsWhere = mQ.getRowsWhere('test_table', { percent: 10 },
-                      function(err, rows) {
-                        if (err) {
-                          console.log(err);
-                        }
-
-                        console.log('testGetRowsWhere:');
-                        console.log(rows);
-                        callback();
-                      });
-                },
-  rowStream: function(callback) {
-           var rowStream = mQ.createRowStream('test_table');
-           var rowStreamT2 = through2.obj(function(row, enc, cb) {
-             console.log('testRowStream: ');
-             console.log(row.percent);
-             cb();
-           },
-           function() {
-             callback();
-           });
-           rowStream.pipe(rowStreamT2);
-         },
-
-  rowStreamWhere: function(callback) {
-            var rowStreamWhere = mQ.createRowStreamWhere('test_table', { percent: 10 });
-            var rowStreamWhereT2 = through2.obj(function(row, enc, cb) {
-              console.log('testRowStreamWhere: ' + row.percent);
-              cb();
-            },
-            function() {
-              callback();
-            });
-            rowStreamWhere.pipe(rowStreamWhereT2);
+    var rowsWhere = mQ.getRowsWhere('test_table', { percent: 10 },
+        function(err, rows) {
+          if (err) {
+            console.log(err);
           }
+
+          console.log('testGetRowsWhere:');
+          console.log(rows);
+          callback();
+        });
+  },
+  rowStream: function(callback) {
+    var rowStream = mQ.createRowStream('test_table');
+    var rowStreamT2 = through2.obj(function(row, enc, cb) {
+      console.log('testRowStream: ');
+      console.log(row.percent);
+      cb();
+    },
+    function() {
+      callback();
+    });
+    rowStream.pipe(rowStreamT2);
+  },
+  rowStreamWhere: function(callback) {
+    var rowStreamWhere = mQ.createRowStreamWhere('test_table', { percent: 10 });
+    var rowStreamWhereT2 = through2.obj(function(row, enc, cb) {
+      console.log('testRowStreamWhere: ' + row.percent);
+      cb();
+    },
+    function() {
+      callback();
+    });
+    rowStreamWhere.pipe(rowStreamWhereT2);
+  }
 }
 
 async.parallel(tests, function(err, results) {
