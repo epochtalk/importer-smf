a4d37cdaa763d34ce277455a07da94f84d47f59c
Bronson Oka <boka@slickage.com>
2014-09-12 02:17:52 -1000

    add bin installer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var path = require('path');
 var pJSON = require(path.join(__dirname, 'package.json'));
 var program = require('commander');
@@ -5,7 +6,6 @@ var program = require('commander');
 program
   .version(pJSON.version)
   .option('--leveldb <path>', 'Path to leveldb (default: ./epoch.db')
-  .option('-d, --debug [options]')
   .option('-v, --verbose [options]', '')
   .option('--color [options]', 'Color the output')
   .option('--users', 'Only import users')
@@ -16,7 +16,6 @@ program
 var imp = require(path.join(__dirname, 'smf-import'));
 var args = {
   db: program.leveldb,
-  debug: program.debug,
   verbose: program.verbose,
   users: program.users,
   forum: program.forum,
