abd179c126f2ba0d6817b794bc07106ca37f88f6
Bronson Oka <boka@slickage.com>
2014-09-16 17:09:54 -1000

    implement user import with new epochImporter

--- smf-import.js
+++ smf-import.js
@@ -6,15 +6,53 @@ module.exports = function smfImport(args, topCallback) {
   var forum = args.forum;
 
   var path = require('path');
-  var importUsers = require(path.join(__dirname,'epoch_importer','import-users'));
-  var importBoards = require(path.join(__dirname,'epoch_importer','import-boards'));
+  var epochImporter = require(path.join(__dirname,'epoch_importer'));
+  var rw = require('rainbow-word');
+  var rainbow = rw.pattern({
+    style: 'bold',
+      save: false
+  });
+  var printStats = function(count) {
+    process.stdout.write(rainbow.convert(
+      'users: ', count.users + ' ',
+      'boards: ', count.boards + ' ',
+      'threads: ', count.threads + ' ',
+      'posts: ', count.posts + ' ',
+      'errors: ', count.errs + '\r'
+    ));
+  };
+
+  var count = {
+    users: 0,
+    boards: 0,
+    threads: 0,
+    posts: 0,
+    errs: 0
+  };
+
+  var importOptions = {
+    dbPath: args.db,
+    mQConfig: require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'))
+  };
+
 
   if (users) {
-    importUsers(args, function(){
-      if (verbose) {
+    epochImporter.importUsers(importOptions, function(err, newUser, callback){
+      if (!quiet) {
+        if(err) {
+          count.errs++;
+        }
+        else {
+          count.users++;
+        }
+        printStats(count);
+      }
+      return callback();
+    },
+    function() {
+      if (!quiet) {
         process.stdout.write('\n');
       }
-      topCallback();
     });
   }
   else if (forum) {
