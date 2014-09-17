2060583c4cc889f879b6fbdf7b2110ea8c2b2cd0
Bronson Oka <boka@slickage.com>
2014-09-16 18:21:21 -1000

    get rid of optional arg shifting in import-threads

--- epoch_import/import-threads.js
+++ epoch_import/import-threads.js
@@ -2,12 +2,6 @@ var path = require('path');
 var through2 = require('through2');
 
 module.exports = function(options, newBoard, handler, callback) {
-  var args = [];
-  for (var i = 0; i < arguments.length; i++) {
-    args.push(arguments[i]);
-  }
-  var callback = args.pop();
-  var handler = args.pop();
   var dbPath = options.db;
   var oldBoardId = newBoard.smf.ID_BOARD;
   var newBoardId = newBoard.id;
