599bb09de66869a9241c9b7bcf3e54417e48b09e
Bronson Oka <boka@slickage.com>
2014-09-16 16:22:17 -1000

    user-import no longer depends on importer

--- epoch_importer/user-importer.js
+++ epoch_importer/user-importer.js
@@ -1,21 +1,47 @@
-module.exports = function(args, callback) {
-  var path = require('path');
+var path = require('path');
+var through2 = require('through2');
+
+module.exports = function(options, handler, callback) {
+  var args = [];
+  for (var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+  var callback = args.pop();
+  var handler = args.pop();
+  var dbPath = options.db();
+
+  // TODO: move || to top level
+  var mysqlConfig = options.mQConfig() || require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'));
+  // TODO: remove this?
+  mysqlConfig.connectionLimit = 1;
+
+  var core = require('epochcore')(dbPath);
   var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
   var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-  var mysqlConfig = require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'));
-  mysqlConfig.connectionLimit = 1;
   var mQ = new MysqlQuerier(mysqlConfig, function(err) {
     if (err) {
       console.log('User Connection Error');
       return callback(err);
     }
   });
-  var Importer = require(path.join(__dirname, 'importer'));
-  var importer = new Importer(args, 'users', function() {
-    process.stdout.write('\n');
-    mQ.end();
-    callback();
-  });
   var userStream = epochStream.createUserStream(mQ);
-  userStream.pipe(importer);  // When stream is empty, worker is done
+  userStream.pipe(through2.obj(function(userObject, enc, trCb) {
+    core.users.import(userObject)
+    .then(function(newUser) {
+      if (handler) {
+        handler(null, newUser, trCb);
+      }
+      else {
+        trCb();
+      }
+    })
+    .catch(function(err){
+      if (handler) {
+        handler(err, null, trCb);
+      }
+      else {
+        trCb();
+      }
+    });
+  }, callback));
 };
