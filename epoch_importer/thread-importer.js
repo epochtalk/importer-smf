3abffd1aa3eb5b3e235d6000f2ee831333a877b9
Bronson Oka <boka@slickage.com>
2014-09-16 16:17:04 -1000

    thread-importer no longer depends on importer.js

--- epoch_importer/thread-importer.js
+++ epoch_importer/thread-importer.js
@@ -1,24 +1,49 @@
 var path = require('path');
-module.exports = function(args, newBoard, callback) {
-  var importPosts = require(path.join(__dirname, 'import-posts'));
+var through2 = require('through2');
+
+module.exports = function(options, newBoard, handler, callback) {
+  var args = [];
+  for (var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+  var callback = args.pop();
+  var handler = args.pop();
+  var dbPath = options.db();
   var oldBoardId = newBoard.smf.ID_BOARD;
   var newBoardId = newBoard.id;
+
+  // TODO: move || to top level
+  var mysqlConfig = options.mQConfig() || require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'));
+  // TODO: remove this?
+  mysqlConfig.connectionLimit = 5;
+
+  var core = require('epochcore')(dbPath);
   var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
   var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-  var mysqlConfig = require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'));
-  mysqlConfig.connectionLimit = 5;
   var mQ = new MysqlQuerier(mysqlConfig, function(err) {
     if (err) {
       console.log('Thread Connection Error');
       return callback(err);
     }
   });
-  var Importer = require(path.join(__dirname, 'importer'));
-  var importer = new Importer(args, 'threads', importPosts, function() {
-    process.stdout.write('\n');
-    mQ.end();
-    callback();
-  });
-  var threadStream = epochStream.createThreadStream(mQ, oldBoardId, newBoardId);
-  threadStream.pipe(importer);  // When stream is empty, worker is done
+  var threadStream = epochStream.threadStream(mQ, oldBoardId, newBoardId);
+  threadStream.pipe(through2.obj(function(threadObject, enc, trCb) {
+    core.threads.import(threadObject)
+    .then(function(newThread) {
+      if (handler) {
+        handler(null, newThread, trCb);
+      }
+      else {
+        trCb();
+      }
+    })
+    .catch(function(err){
+      if (handler) {
+        handler(err, null, trCb);
+      }
+      else {
+        trCb();
+      }
+    });
+  }, callback));
 };
