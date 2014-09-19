2555453e452352157c7e8db98df608a9ea6c203e
Bronson Oka <boka@slickage.com>
2014-09-18 19:24:42 -1000

    remove separatation for users and forum in smf-import

--- smf-import.js
+++ smf-import.js
@@ -41,30 +41,23 @@ module.exports = function smfImport(args, topCallback) {
   };
 
 
-  if (users) {
-    epochImport.users(options, function(err, newUser, userCb) {
-      if(err) {
-        count.errs++;
-        if (log) {
-          logfile.write('User error:\n');
-          logfile.write(err.toString()+'\n');
-        }
+  epochImport.users(options, function(err, newUser, userCb) {
+    if(err) {
+      count.errs++;
+      if (log) {
+        logfile.write('User error:\n');
+        logfile.write(err.toString()+'\n');
       }
-      else {
-        count.users++;
-      }
-      if (!quiet) {
-        printStats(count);
-      }
-      return userCb();
-    },
-    function() {
-      if (!quiet) {
-        process.stdout.write('\n');
-      }
-    });
-  }
-  else if (forum) {
+    }
+    else {
+      count.users++;
+    }
+    if (!quiet) {
+      printStats(count);
+    }
+    return userCb();
+  },
+  function() {
     epochImport.boards(options, function(err, newBoard, boardCb) {
       if(err) {
         count.errs++;
@@ -119,79 +112,5 @@ module.exports = function smfImport(args, topCallback) {
         process.stdout.write('\n');
       }
     });
-  }
-  else {
-    epochImport.users(options, function(err, newUser, userCb) {
-      if(err) {
-        count.errs++;
-        if (log) {
-          logfile.write('User error:\n');
-          logfile.write(err.toString()+'\n');
-        }
-      }
-      else {
-        count.users++;
-      }
-      if (!quiet) {
-        printStats(count);
-      }
-      return userCb();
-    },
-    function() {
-      epochImport.boards(options, function(err, newBoard, boardCb) {
-        if(err) {
-          count.errs++;
-          if (log) {
-            logfile.write('Board error:\n');
-            logfile.write(err.toString()+'\n');
-          }
-          return boardCb();
-        }
-        else {
-          count.boards++;
-          epochImport.threads(options, newBoard, function(err, newThread, threadCb) {
-            if(err) {
-              count.errs++;
-              if (log) {
-                logfile.write('Thread error:\n');
-                logfile.write(err.toString()+'\n');
-              }
-              return threadCb();
-            }
-            else {
-              count.threads++;
-              epochImport.posts(options, newThread, function(err, newPost, postCb) {
-                if(err) {
-                  count.errs++;
-                  if (log) {
-                    logfile.write('Post error:\n');
-                    logfile.write(err.toString()+'\n');
-                  }
-                }
-                else {
-                  count.posts++;
-                }
-                if (!quiet) {
-                  printStats(count);
-                }
-                return postCb();
-              }, threadCb);
-            }
-            if (!quiet) {
-              printStats(count);
-            }
-          }, boardCb);
-
-        }
-        if (!quiet) {
-          printStats(count);
-        }
-      },
-      function() {
-        if (!quiet) {
-          process.stdout.write('\n');
-        }
-      });
-    });
-  }
+  });
 };
