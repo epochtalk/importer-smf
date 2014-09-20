9f7fe814062c8f15adf8b417617c387081142b90
Bronson Oka <boka@slickage.com>
2014-09-19 19:11:05 -1000

    add import-categories

---
+++ epoch_import/import-categories.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var through2 = require('through2');
+
+module.exports = function(options, handler, callback) {
+  var mysqlConfig = options.mQConfig;
+  // TODO: remove this?
+  mysqlConfig.connectionLimit = 1;
+
+  var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
+  var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
+  var mQ = new MysqlQuerier(mysqlConfig, function(err) {
+    if (err) {
+      return callback(err);
+    }
+  });
+
+  var categoryStream = epochStream.createCategoryStream(mQ);
+  categoryStream.pipe(through2.obj(function(categoryObject, enc, trCb) {
+    handler(null, categoryObject, trCb);
+  }, function() {
+    mQ.end();
+    callback();
+  }));
+};
--- epoch_import/index.js
+++ epoch_import/index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var epochImport = {};
 
+epochImport.categories = require(path.join(__dirname, 'import-categories'));
 epochImport.users = require(path.join(__dirname, 'import-users'));
 epochImport.boards = require(path.join(__dirname, 'import-boards'));
 epochImport.threads = require(path.join(__dirname, 'import-threads'));
