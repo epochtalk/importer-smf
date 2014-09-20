dea666de674359dea53fdde0dfe4ebd815743cbd
Bronson Oka <boka@slickage.com>
2014-09-19 19:10:32 -1000

    add category-stream

---
+++ epoch_stream/category-stream.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var EpochCollection = require(path.join(__dirname, 'epoch-collection'));
+var through2 = require('through2');
+
+module.exports = function(mQ) {
+  var table = 'smf_categories';
+  var tableMapSafe = {
+    name: 'name'
+  };
+  var smfMap = [
+    'ID_CAT'
+  ];
+
+  var options = {};
+  options.orderBy = 'catOrder';
+
+  var rowStream = mQ.createRowStream(table, options);
+  var tr = through2.obj(function(row, enc, cb) {
+    var epochCollection = new EpochCollection();
+    epochCollection.map(row, tableMapSafe, {validate: true});
+    epochCollection.subMap(row, smfMap, {key: 'smf'});
+    this.push(epochCollection.collection);
+    return cb();
+  });
+  var categoryStream = rowStream.pipe(tr); return categoryStream;
+};
--- epoch_stream/index.js
+++ epoch_stream/index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 module.exports = {
+  createCategoryStream: require(path.join(__dirname, 'category-stream')),
   createUserStream: require(path.join(__dirname, 'user-stream')),
   createBoardStream: require(path.join(__dirname, 'board-stream')),
   createThreadStream: require(path.join(__dirname, 'thread-stream')),
