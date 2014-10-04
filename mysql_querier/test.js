386c8d9d44e05a96c482d7cea7bd060aa1a4297d
Bronson Oka <boka@slickage.com>
2014-10-04 13:55:40 -1000

    update test to work with new querier

--- mysql_querier/test.js
+++ mysql_querier/test.js
@@ -3,41 +3,89 @@ var MysqlQuerier = require(path.join(__dirname, 'mysql-querier'));
 var config = require(path.join(__dirname, 'config.json'));
 var async = require('async');
 var through2 = require('through2');
-var mQ = null;
+var mQ = new MysqlQuerier(config);
 
 var tests = [
   function(callback) {
-    mQ = new MysqlQuerier(config, callback);
+    var querier = mQ.getQuerier(function(err) {
+      if (err) {
+        callback(err);
+      }
+      querier.getTables(function(err, results) {
+        querier.release();
+        return callback(err, results);
+      });
+    });
   },
   function(callback) {
-    console.log('asodijsadicjo');
-    mQ.getTables(callback);
+    var querier = mQ.getQuerier(function(err) {
+      if (err) {
+        callback(err);
+      }
+      querier.getColumns('test_table', function(err, results) {
+        querier.release();
+        return callback(err, results);
+      });
+    });
   },
   function(callback) {
-    mQ.getColumns('test_table', callback);
+    var querier = mQ.getQuerier(function(err) {
+      if (err) {
+        callback(err);
+      }
+      querier.getRowsWhere('test_table', {percent: 10}, function(err, results) {
+        querier.release();
+        return callback(err, results);
+      });
+    });
   },
   function(callback) {
-    var rowsWhere = mQ.getRowsWhere('test_table', { percent: 10 }, callback);
+    var querier = mQ.getQuerier(function(err) {
+      if (err) {
+        callback(err);
+      }
+      var results = [];
+      var rowStream = querier.createRowStream('test_table');
+      rowStream.pipe(through2.obj(function(row, enc, cb) {
+        results.push(row.percent);
+        cb();
+      }, function() {
+        querier.release();
+        return callback(err, results);
+      }));
+    });
   },
   function(callback) {
-    var rowStream = mQ.createRowStream('test_table');
-    var rowStreamT2 = through2.obj(function(row, enc, cb) {
-      console.log('testRowStream: ');
-      console.log(row.percent);
-      cb();
-    }, callback);
-    rowStream.pipe(rowStreamT2);
+    var querier = mQ.getQuerier(function(err) {
+      if (err) {
+        callback(err);
+      }
+      var results = [];
+      var rowStream = querier.createRowStream('test_table', {orderBy: 'percent'});
+      rowStream.pipe(through2.obj(function(row, enc, cb) {
+        results.push(row.percent);
+        cb();
+      }, function() {
+        querier.release();
+        return callback(err, results);
+      }));
+    });
   },
   function(callback) {
-    var rowStreamWhere = mQ.createRowStreamWhere('test_table', { percent: 10 });
-    var rowStreamWhereT2 = through2.obj(function(row, enc, cb) {
-      console.log('testRowStreamWhere: ' + row.percent);
-      cb();
-    }, callback);
-    rowStreamWhere.pipe(rowStreamWhereT2);
-  },
-  function(callback) {
-    mQ.end(callback);
+    var querier = mQ.getQuerier(function(err) {
+      if (err) {
+        callback(err);
+      }
+      var results = [];
+      var rowStream = querier.createRowStream('test_table', {where: {'percent': 10}});
+      rowStream.pipe(through2.obj(function(row, enc, cb) {
+        results.push(row.percent);
+        cb();
+      }, function() {
+        querier.release();
+        return callback(err, results);
+      }));
+    });
   }
 ];
 
@@ -48,4 +96,5 @@ async.series(tests, function(err, results) {
   else {
     console.log(results);
   }
+  mQ.end();
 });
