8f0fadec36175dfb6fb0cfa2b8e40290331daaee
Bronson Oka <boka@slickage.com>
2014-10-04 17:21:34 -1000

    update test userstream

--- test_drivers/test-user-stream.js
+++ test_drivers/test-user-stream.js
@@ -1,17 +1,17 @@
 var path = require('path');
 var through2 = require('through2');
 var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
-var mysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-var mQConfig = require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'));
-var mQ = mysqlQuerier(mQConfig);
+var mQ = require(path.join(__dirname, '..', 'mq'));
+var querier = mQ.getQuerier(function(err) {
+  var userStream = epochStream.createUserStream(querier);
 
-var userStream = epochStream.createUserStream(mQ);
-
-userStream.pipe(through2.obj(function (data, enc, cb) {
-  console.log('test: ');
-  console.log(data);
-  return cb();
-},
-function() {
-  mQ.end();
-}));
+  userStream.pipe(through2.obj(function (data, enc, cb) {
+    console.log('test: ');
+    console.log(data);
+    return cb();
+  },
+  function() {
+    querier.release();
+    mQ.end();
+  }));
+});
