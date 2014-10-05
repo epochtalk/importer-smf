fb9a51add474a474ca9fabb34e0c61d7a001c254
Bronson Oka <boka@slickage.com>
2014-10-04 16:50:00 -1000

    handle eror on import-categories

--- epoch_import/import-categories.js
+++ epoch_import/import-categories.js
@@ -6,6 +6,9 @@ var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
 
 module.exports = function(handler, callback) {
   var querier = mQ.getQuerier(function(err) {
+    if (err) {
+      return callback(err);
+    }
     var categoryStream = epochStream.createCategoryStream(querier);
     categoryStream.pipe(through2.obj(function(categoryObject, enc, trCb) {
       handler(null, categoryObject, trCb);
