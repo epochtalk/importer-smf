e8c2092875e0f084890db0b3a2a28f2a84fcafd3
Bronson Oka <boka@slickage.com>
2014-10-04 17:22:00 -1000

    update import-users to work with new implementation

--- epoch_import/import-users.js
+++ epoch_import/import-users.js
@@ -1,54 +1,50 @@
 var path = require('path');
 var through2 = require('through2');
 var args = require(path.join(__dirname, '..', 'args'));
+var mQ = require(path.join(__dirname, '..', 'mq'));
 var db = require(path.join(__dirname, '..', 'db'));
+var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
 
 module.exports = function(handler, callback) {
-  var mysqlConfig = args.mysqlConfig;
-  mysqlConfig.connectionLimit = 1;
-
-  var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
-  var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-  var mQ = new MysqlQuerier(mysqlConfig, function(err) {
+  var querier = mQ.getQuerier(function(err) {
     if (err) {
       return callback(err);
     }
-  });
-
-  // "deleted" user
-  db.users.import({
-    username: 'deleted',
-    email: 'deleted',
-    smf: {
-      ID_MEMBER: -1
-    }
-  })
-  .catch(function(err) {
-    console.log('"deleted" user error:');
-    console.log(err);
-  });
-
-  var userStream = epochStream.createUserStream(mQ);
-  userStream.pipe(through2.obj(function(userObject, enc, trCb) {
-    db.users.import(userObject)
-    .then(function(newUser) {
-      if (handler) {
-        handler(null, newUser, trCb);
-      }
-      else {
-        trCb();
+    // "deleted" user
+    db.users.import({
+      username: 'deleted',
+      email: 'deleted',
+      smf: {
+        ID_MEMBER: -1
       }
     })
-    .catch(function(err){
-      if (handler) {
-        handler(err, userObject, trCb);
-      }
-      else {
-        trCb();
-      }
+    .catch(function(err) {
+      console.log('"deleted" user error:');
+      console.log(err);
     });
-  }, function() {
-    mQ.end();
-    callback();
-  }));
+
+    var userStream = epochStream.createUserStream(querier);
+    userStream.pipe(through2.obj(function(userObject, enc, trCb) {
+      db.users.import(userObject)
+      .then(function(newUser) {
+        if (handler) {
+          handler(null, newUser, trCb);
+        }
+        else {
+          trCb();
+        }
+      })
+      .catch(function(err){
+        if (handler) {
+          handler(err, userObject, trCb);
+        }
+        else {
+          trCb();
+        }
+      });
+    }, function() {
+      querier.release();
+      callback();
+    }));
+  });
 };
