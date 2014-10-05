252f651c53ded60977d37867e3460a9d50809f03
Bronson Oka <boka@slickage.com>
2014-10-04 16:13:52 -1000

    update category import to work with new mysql-querier

--- epoch_import/import-categories.js
+++ epoch_import/import-categories.js
@@ -1,24 +1,17 @@
 var path = require('path');
 var through2 = require('through2');
 var args = require(path.join(__dirname, '..', 'args'));
+var mQ = require(path.join(__dirname, '..', 'mq'));
+var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
 
 module.exports = function(handler, callback) {
-  var mysqlConfig = args.mysqlConfig;
-  mysqlConfig.connectionLimit = 1;
-
-  var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
-  var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-  var mQ = new MysqlQuerier(mysqlConfig, function(err) {
-    if (err) {
-      return callback(err);
-    }
+  var querier = mQ.getQuerier(function(err) {
+    var categoryStream = epochStream.createCategoryStream(querier);
+    categoryStream.pipe(through2.obj(function(categoryObject, enc, trCb) {
+      handler(null, categoryObject, trCb);
+    }, function() {
+      querier.release();
+      return callback();
+    }));
   });
-
-  var categoryStream = epochStream.createCategoryStream(mQ);
-  categoryStream.pipe(through2.obj(function(categoryObject, enc, trCb) {
-    handler(null, categoryObject, trCb);
-  }, function() {
-    mQ.end();
-    callback();
-  }));
 };
