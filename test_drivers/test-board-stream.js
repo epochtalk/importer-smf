83e654be9df04c11f27c537b31dd0e98bd118a25
Bronson Oka <boka@slickage.com>
2014-10-04 16:16:15 -1000

    update boardstream test to work with new implementation

--- test_drivers/test-board-stream.js
+++ test_drivers/test-board-stream.js
@@ -1,17 +1,17 @@
 var path = require('path');
 var through2 = require('through2');
 var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
-var mysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-var mQConfig = require(path.join(process.env.HOME,'.epoch_admin', 'mysql-config'));
-var mQ = mysqlQuerier(mQConfig);
+var mQ = require(path.join(__dirname, '..', 'mq'));
+var querier = mQ.getQuerier(function(err) {
+  var boardStream = epochStream.createBoardStream(querier);
 
-var boardStream = epochStream.createBoardStream(mQ);
-
-boardStream.pipe(through2.obj(function (data, enc, cb) {
-  console.log('test: ');
-  console.log(data);
-  return cb();
-},
-function() {
-  mQ.end();
-}));
+  boardStream.pipe(through2.obj(function (data, enc, cb) {
+    console.log('test: ');
+    console.log(data);
+    cb();
+  },
+  function() {
+    querier.release();
+    mQ.end();
+  }));
+});
