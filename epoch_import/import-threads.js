02099109d55b5dfea2b7c6eb5d1903e52b4377ae
Bronson Oka <boka@slickage.com>
2014-10-07 12:12:18 -1000

    update import-threads to work with new implementation
    handles two queriers along with new mysql-querier
    implementation

--- epoch_import/import-threads.js
+++ epoch_import/import-threads.js
@@ -1,43 +1,45 @@
 var path = require('path');
 var through2 = require('through2');
 var args = require(path.join(__dirname, '..', 'args'));
+var mQ = require(path.join(__dirname, '..', 'mq'));
 var db = require(path.join(__dirname, '..', 'db'));
+var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
 
 module.exports = function(newBoard, handler, callback) {
-  var oldBoardId = newBoard.smf.ID_BOARD;
-  var newBoardId = newBoard.id;
-
-  var mysqlConfig = args.mysqlConfig;
-  mysqlConfig.connectionLimit = 2;
-
-  var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
-  var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-  var mQ = new MysqlQuerier(mysqlConfig, function(err) {
+  var querier = mQ.getQuerier(function(err) {
     if (err) {
       return callback(err);
     }
-  });
-  var threadStream = epochStream.createThreadStream(mQ, oldBoardId, newBoardId);
-  threadStream.pipe(through2.obj(function(threadObject, enc, trCb) {
-    db.threads.import(threadObject)
-    .then(function(newThread) {
-      if (handler) {
-        handler(null, newThread, trCb);
+    var msgQuerier = mQ.getQuerier(function(err) {
+      if (err) {
+        return callback(err);
       }
-      else {
-        trCb();
-      }
-    })
-    .catch(function(err){
-      if (handler) {
-        handler(err, threadObject, trCb);
-      }
-      else {
-        trCb();
-      }
+      var oldBoardId = newBoard.smf.ID_BOARD;
+      var newBoardId = newBoard.id;
+      var threadStream = epochStream.createThreadStream(querier, msgQuerier, oldBoardId, newBoardId);
+      threadStream.pipe(through2.obj(function(threadObject, enc, trCb) {
+        db.threads.import(threadObject)
+        .then(function(newThread) {
+          if (handler) {
+            handler(null, newThread, trCb);
+          }
+          else {
+            trCb();
+          }
+        })
+        .catch(function(err){
+          if (handler) {
+            handler(err, threadObject, trCb);
+          }
+          else {
+            trCb();
+          }
+        });
+      }, function() {
+        msgQuerier.release();
+        querier.release();
+        return callback();
+      }));
     });
-  }, function() {
-    mQ.end();
-    callback();
-  }));
+  });
 };
