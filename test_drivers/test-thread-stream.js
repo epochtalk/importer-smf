2cc7de77284016c2fd8c067d83e8144e34686962
Bronson Oka <boka@slickage.com>
2014-10-07 12:09:46 -1000

    update thread test to work with inner stream

--- test_drivers/test-thread-stream.js
+++ test_drivers/test-thread-stream.js
@@ -3,15 +3,21 @@ var through2 = require('through2');
 var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
 var mQ = require(path.join(__dirname, '..', 'mq'));
 var querier = mQ.getQuerier(function(err) {
-  var threadStream = epochStream.createThreadStream(querier, 86, 0);
+  if (err) {
+    console.log('ERRROR!');
+    console.log(err);
+  }
+  var otherQuerier = mQ.getQuerier(function(err) {
+    var threadStream = epochStream.createThreadStream(querier, otherQuerier, 1, 0);
 
-  threadStream.pipe(through2.obj(function (data, enc, cb) {
-    console.log('test: ');
-    console.log(data);
-    return cb();
-  },
-  function () {
-    querier.release();
-    mQ.end();
-  }));
+    threadStream.pipe(through2.obj(function (data, enc, cb) {
+      console.log('test: ');
+      console.log(data);
+      return cb();
+    },
+    function () {
+      querier.release();
+      mQ.end();
+    }));
+  });
 });
