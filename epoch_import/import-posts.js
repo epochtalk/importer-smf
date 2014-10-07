ea20e0855c09c8a61d62348e6a295c8f41b60681
Bronson Oka <boka@slickage.com>
2014-10-07 12:25:45 -1000

    update importposts to work with new implementation

--- epoch_import/import-posts.js
+++ epoch_import/import-posts.js
@@ -1,43 +1,39 @@
 var path = require('path');
 var through2 = require('through2');
 var args = require(path.join(__dirname, '..', 'args'));
+var mQ = require(path.join(__dirname, '..', 'mq'));
 var db = require(path.join(__dirname, '..', 'db'));
+var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
 
 module.exports = function(newThread, handler, callback) {
-  var oldThreadId = newThread.smf.ID_TOPIC;
-  var newThreadId = newThread.id;
-
-  var mysqlConfig = args.mysqlConfig;
-  mysqlConfig.connectionLimit = 1;
-
-  var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
-  var MysqlQuerier = require(path.join(__dirname, '..', 'mysql_querier'));
-  var mQ = new MysqlQuerier(mysqlConfig, function(err) {
+  var querier = mQ.getQuerier(function(err) {
     if (err) {
       return callback(err);
     }
+    var oldThreadId = newThread.smf.ID_TOPIC;
+    var newThreadId = newThread.id;
+    var postStream = epochStream.createPostStream(querier, oldThreadId, newThreadId);
+    postStream.pipe(through2.obj(function(postObject, enc, trCb) {
+      db.posts.import(postObject)
+      .then(function(newPost) {
+        if (handler) {
+          handler(null, newPost, trCb);
+        }
+        else {
+          trCb();
+        }
+      })
+      .catch(function(err){
+        if (handler) {
+          handler(err, postObject, trCb);
+        }
+        else {
+          trCb();
+        }
+      });
+    }, function() {
+      querier.release();
+      callback();
+    }));
   });
-  var postStream = epochStream.createPostStream(mQ, oldThreadId, newThreadId);
-  postStream.pipe(through2.obj(function(postObject, enc, trCb) {
-    db.posts.import(postObject)
-    .then(function(newPost) {
-      if (handler) {
-        handler(null, newPost, trCb);
-      }
-      else {
-        trCb();
-      }
-    })
-    .catch(function(err){
-      if (handler) {
-        handler(err, postObject, trCb);
-      }
-      else {
-        trCb();
-      }
-    });
-  }, function() {
-    mQ.end();
-    callback();
-  }));
 };
