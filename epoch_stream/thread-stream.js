2070f6aa27bfea187c37442051b60cba369cc1f8
Bronson Oka <boka@slickage.com>
2014-10-20 13:26:09 -1000

    [treesdb] thread stream no longer depends on anything
    * does not depend on parent board
    
    * does not use first message

--- epoch_stream/thread-stream.js
+++ epoch_stream/thread-stream.js
@@ -2,14 +2,10 @@ var path = require('path');
 var EpochCollection = require(path.join(__dirname, 'epoch-collection'));
 var through2 = require('through2');
 
-module.exports = function(querier, msgQuerier, oldBoardId, newBoardId) {
+module.exports = function(querier) {
   var table = 'smf_topics';
   var tableMapSafe = {
     numViews : 'view_count',
-  };
-  var timeMapSafe = {
-    modifiedTime: 'updated_at',
-    posterTime: 'created_at'
   };
 
   var smfMap = [
@@ -17,49 +13,17 @@ module.exports = function(querier, msgQuerier, oldBoardId, newBoardId) {
     'ID_FIRST_MSG'
   ];
 
-  var messageColumns = [
-    'icon',
-    'ID_BOARD',
-    'posterTime',
-    'ID_MSG',
-    'modifiedTime',
-    'ID_MEMBER',
-    'subject',
-    'body',
-    'modifiedName',
-    'posterName',
-    'ID_TOPIC',
-    'ID_MSG_MODIFIED',
-    'smileysEnabled'
-  ];
-
   var options = {};
-  options.where = {ID_BOARD : oldBoardId};
   options.orderBy = 'ID_TOPIC';
 
   var rowStream = querier.createRowStream(table, options);
   var tr = through2.obj(function(row, enc, cb) {
-    var self = this;
     var epochCollection = new EpochCollection();
-    var messageOptions = {
-      where: {ID_MSG: row.ID_FIRST_MSG},
-      columns: messageColumns
-    };
-    var messageStream = msgQuerier.createRowStream('smf_messages', messageOptions);
-    messageStream.pipe(through2.obj(function(firstPost, enc, messageCb) {
       epochCollection.add('type', 'thread');
       epochCollection.map(row, tableMapSafe, {validate: true});
       epochCollection.subMap(row, smfMap, {key: 'smf'});
-      epochCollection.add('board_id', newBoardId);
-      epochCollection.mapTime(firstPost, timeMapSafe, {validate: true});
-      messageCb();
-    }, function() {
-      // TODO:  figure out what to do on missing first post
-      if (epochCollection.collection.smf) {
-        self.push(epochCollection.collection);
-      }
-      cb();
-    }));
+      this.push(epochCollection.collection);
+      return cb();
   });
   var threadStream = rowStream.pipe(tr);
 
