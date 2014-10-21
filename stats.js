0b48900064a93d60eeb2ee6950c3c4ec19d88655
Bronson Oka <boka@slickage.com>
2014-10-20 16:13:54 -1000

    [treesdb] stats now includes category

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
 var path = require('path');
 var StatLogger = require(path.join(__dirname, 'stat-logger'));
-var statFields = ['users', 'boards', 'threads', 'posts', 'errors'];
+var statFields = ['categories', 'users', 'boards', 'threads', 'posts', 'errors'];
 var statLogger = new StatLogger({fields: statFields});
 module.exports = statLogger;
