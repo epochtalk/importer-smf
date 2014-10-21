8ff2cd2c8777f5adf23fde7e295a2e7171c9ef47
Bronson Oka <boka@slickage.com>
2014-10-20 16:09:33 -1000

    [treesdb] importer index keeps an array of all imports

--- epoch_import/index.js
+++ epoch_import/index.js
@@ -6,5 +6,12 @@ epochImport.users = require(path.join(__dirname, 'import-users'));
 epochImport.boards = require(path.join(__dirname, 'import-boards'));
 epochImport.threads = require(path.join(__dirname, 'import-threads'));
 epochImport.posts = require(path.join(__dirname, 'import-posts'));
+epochImport.imports = [
+  epochImport.categories,
+  epochImport.users,
+  epochImport.boards,
+  epochImport.threads,
+  epochImport.posts
+];
 
 module.exports = epochImport;
