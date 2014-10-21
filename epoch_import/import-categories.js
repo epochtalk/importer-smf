2ec3ddb1fc822d25ef848ba7110f5f54a01e328c
Bronson Oka <boka@slickage.com>
2014-10-20 16:09:08 -1000

    [treesdb] category import no longer uses a handler
    * handles its own error logging
    
    * handles its own stat logging

--- epoch_import/import-categories.js
+++ epoch_import/import-categories.js
@@ -2,19 +2,36 @@ var path = require('path');
 var through2 = require('through2');
 var args = require(path.join(__dirname, '..', 'args'));
 var mQ = require(path.join(__dirname, '..', 'mq'));
+var db = require(path.join(__dirname, '..', 'db'));
 var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
+var logfile = require(path.join(__dirname, '..', 'log'));
+var statLogger = require(path.join(__dirname, '..', 'stats'));
 
-module.exports = function(handler, callback) {
+module.exports = function(callback) {
   var querier = mQ.getQuerier(function(err) {
     if (err) {
+      statLogger.increment('errors');
+      logfile.write('Category connection:');
+      logfile.write(err.toString());
       return callback(err);
     }
     var categoryStream = epochStream.createCategoryStream(querier);
     categoryStream.pipe(through2.obj(function(categoryObject, enc, trCb) {
-      handler(null, categoryObject, trCb);
+      db.store(categoryObject, function(err, newCategory) {
+        if (err) {
+          statLogger.increment('errors');
+          logfile.write('Category: ' + categoryObject.smf.ID_CAT + '\n');
+          logfile.write(err.toString() + '\n');
+        }
+        else {
+          statLogger.increment('categories');
+        }
+        return trCb();
+      });
     }, function() {
+      statLogger.tag('categories', '(finished)');
       querier.release();
-      return callback();
+      return callback(null, 'Category import succeded');
     }));
   });
 };
