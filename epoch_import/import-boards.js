41e5591f153f6659eba471a0877945c3f877ed43
Bronson Oka <boka@slickage.com>
2014-10-20 15:43:37 -1000

    [treesdb] board import no longer uses a handler
    * handles its own error logging
    
    * handles its own stat logging
    
    * does not need to spawn thread imports

--- epoch_import/import-boards.js
+++ epoch_import/import-boards.js
@@ -4,30 +4,34 @@ var args = require(path.join(__dirname, '..', 'args'));
 var mQ = require(path.join(__dirname, '..', 'mq'));
 var db = require(path.join(__dirname, '..', 'db'));
 var epochStream = require(path.join(__dirname, '..', 'epoch_stream'));
+var logfile = require(path.join(__dirname, '..', 'log'));
+var statLogger = require(path.join(__dirname, '..', 'stats'));
 
-module.exports = function(handler, callback) {
+module.exports = function(callback) {
   var querier = mQ.getQuerier(function(err) {
     if (err) {
+      statLogger.increment('errors');
+      logfile.write('Board connection:');
+      logfile.write(err.toString());
       return callback(err);
     }
     var boardStream = epochStream.createBoardStream(querier);
     boardStream.pipe(through2.obj(function(boardObject, enc, trCb) {
       db.store(boardObject, function(err, newBoard) {
-        if (handler) {
-          if (err) {
-            handler(err, boardObject, trCb);
-          }
-          else {
-            handler(err, newBoard, trCb);
-          }
+        if (err) {
+          statLogger.increment('errors');
+          logfile.write('Board: ' + boardObject.smf.ID_BOARD + '\n');
+          logfile.write(err.toString() + '\n');
         }
         else {
-          trCb();
+          statLogger.increment('boards');
         }
+        return trCb();
       });
     }, function() {
+      statLogger.tag('boards', '(finished)');
       querier.release();
-      return callback();
+      return callback(null, 'Board import succeded');
     }));
   });
 };
